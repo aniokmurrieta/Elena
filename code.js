6371fafabf72f9df670d8d3a13dc5cad2c3f5a33
aniokmurrieta <amlosalamancados@gmail.com>
2025-02-25 18:26:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -58,4 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
             sendButton.click();
         }
     });
+    if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(() => console.log("Service Worker registrado"))
+        .catch(err => console.log("Error al registrar el Service Worker", err));
+}
 });
